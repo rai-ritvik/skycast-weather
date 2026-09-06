d12a97a78308ef33cb5302df4a423f66ef728817
Ritvik Rai <rit.rai2797@gmail.com>
2026-09-06 14:12:52 +0530

    feat: add features to the script

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const cityInput = document.getElementById("cityInput");
 const cityNameDisplay = document.getElementById("cityName");
 const tempDisplay = document.getElementById("temperature");
 const statusDisplay = document.getElementById("statusMsg")
+const mainIconDisplay = document.getElementById("mainIcon");
+const feelsLikeDisplay = document.getElementById("feelsLike");
+const humidityDisplay = document.getElementById("humidity");
+const windSpeedDisplay = document.getElementById("windSpeed");
+const pressureDisplay = document.getElementById("pressure");
+const visibilityDisplay = document.getElementById("visibility");
+const lastUpdatedDisplay = document.getElementById("lastUpdated");
 
 async function doSearch() {
     const typedCity = cityInput.value;
@@ -13,6 +20,7 @@ async function doSearch() {
         tempDisplay.textContent = "";
 
         const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${typedCity}&count=1`);
+
         const data = await response.json();
 
         if (!data.results) {
@@ -22,16 +30,33 @@ async function doSearch() {
         const lat = data.results[0].latitude;
         const lon = data.results[0].longitude;
 
-        const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`);
+        const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,apparent_temperature,relative_humidity_2m,weather_code,surface_pressure,visibility,wind_speed_10m,wind_direction_10m`);
+
+
         const weatherData = await weatherResponse.json();
 
         console.log(data);
         console.log(weatherData);
 
         const foundCity = data.results[0].name;
-        const temperature = weatherData.current_weather.temperature;
+
         cityNameDisplay.textContent = foundCity;
-        tempDisplay.textContent = temperature + " °C";
+
+        tempDisplay.textContent = `${weatherData.current.temperature_2m} °C`;
+
+        feelsLikeDisplay.textContent = `${weatherData.current.apparent_temperature} °C`;
+
+        humidityDisplay.textContent = `${weatherData.current.relative_humidity_2m}%`;
+
+        windSpeedDisplay.textContent = `${weatherData.current.wind_speed_10m}km/h`;
+
+        pressureDisplay.textContent = `${weatherData.current.surface_pressure}hPa`;
+
+        visibilityDisplay.textContent = `${weatherData.current.visibility / 1000}km`;
+
+        lastUpdatedDisplay.textContent = weatherData.current.time.split("T")[1];
+
+        mainIconDisplay.className = getWeatherIcon(weatherData.current.weather_code);
     }
 
     catch (error) {
@@ -47,3 +72,24 @@ cityInput.addEventListener("keydown", (e) => {
         doSearch();
     }
 });
+
+function getWeatherIcon(weatherCode) {
+    if (weatherCode === 0) {
+        return "fa-solid fa-sun";
+    }
+    else if (weatherCode >= 1 && weatherCode <= 3) {
+        return "fa-solid fa-cloud";
+    }
+    else if ((weatherCode >= 51 && weatherCode <= 67) || (weatherCode >= 80 && weatherCode <= 82)) {
+        return "fa-solid fa-cloud-rain";
+    }
+    else if (weatherCode >= 71 && weatherCode <= 86) {
+        return "fa-solid fa-snowflake";
+    }
+    else if (weatherCode >= 95) {
+        return "fa-solid fa-bolt";
+    }
+    else {
+        return "fa-solid fa-smog";
+    }
+}
